30ef7f589d2fbe668dee78196d99c00e8cf00ff2
chung isaac <isaac815@naver.com>
2024-08-12 18:26:11 +0900

    back

---
+++ 백엔드/coplate/static/coplate/js/auth.js
@@ -0,0 +1,14 @@
+{
+  const elts = document.querySelectorAll('[data-login-required]');
+
+  function handleClick(e) {
+    if (!isAuthenticated) {
+      e.preventDefault();
+      alert('로그인이 필요합니다');
+    }
+  }
+
+  for (let elt of elts){
+    elt.addEventListener('click',handleClick);
+  }
+}
