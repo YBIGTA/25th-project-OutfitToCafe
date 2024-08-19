87540176eba79ed54cabd123ccb39c6bb683f1b1
Minseo Kim <lolz77@yonsei.ac.kr>
2024-08-19 15:21:04 +0900

    유사도계산

---
+++ 유사도계산/functions.py
@@ -0,0 +1,44 @@
+import torch
+from transformers import AutoTokenizer, AutoModel
+import os
+from sklearn.metrics.pairwise import cosine_similarity
+
+# KoBERT 토크나이저와 모델 로드
+tokenizer = AutoTokenizer.from_pretrained('skt/kobert-base-v1')
+model = AutoModel.from_pretrained('skt/kobert-base-v1')
+
+# BERT 임베딩 생성 함수
+def get_bert_embedding(text):
+    inputs = tokenizer(text, return_tensors='pt', max_length=512, truncation=True, padding='max_length')
+    
+    # Ensure token_type_ids are within bounds
+    if 'token_type_ids' in inputs:
+        inputs['token_type_ids'] = torch.zeros_like(inputs['input_ids'])
+
+    with torch.no_grad():
+        outputs = model(**inputs)
+
+    # [CLS] 토큰의 벡터 추출
+    return outputs.last_hidden_state[:, 0, :].numpy()
+
+# # 테스트용 코드
+# text = "한국어 모델을 테스트합니다."
+# embedding = get_bert_embedding(text)
+# print(embedding)
+
+# 모든 파일 가져오기 
+def get_all_file_lists(directory):
+    return [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
+
+def jaccard_similarity(set1, set2):
+    # 집합으로 변환
+    set1 = set(set1) if isinstance(set1, str) else set1
+    set2 = set(set2) if isinstance(set2, str) else set2
+
+    intersection = len(set1.intersection(set2))
+    union = len(set1.union(set2))
+    if union == 0:
+        return 0.0
+    return intersection / union
+
+
